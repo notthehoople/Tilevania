ac77ec650234a2d36235551c677bca0053e76736
Mark Bradshaw <notthehoople@googlemail.com>
2020-06-12 17:19:55 +0100

    Added Ladders
    - player can climb ladders
    - ladder tile added to tile palette
    - ladder layer added to allow IsTrigger to be set and prevent the player from colliding with the ladder
    - added Climb() method to player.cs

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -7,41 +7,38 @@ public class Player : MonoBehaviour
     // Configuration Parameters
     [SerializeField] float moveSpeed = 5f;
     [SerializeField] float jumpSpeed = 5f;
+    [SerializeField] float climbSpeed = 5f;
 
     // Cached References
-    Rigidbody2D myRigidBody;
+    Rigidbody2D playerRigidBody;
     Animator playerAnimator;
     Collider2D playerCollider2D;
+    float gravityScaleAtStart;
 
     // Methods
     private void Start()
     {
-        myRigidBody = GetComponent<Rigidbody2D>();
+        playerRigidBody = GetComponent<Rigidbody2D>();
         playerAnimator = GetComponent<Animator>();
         playerCollider2D = GetComponent<Collider2D>();
+        gravityScaleAtStart = playerRigidBody.gravityScale;
     }
 
     private void Update()
     {
         Move();
         Jump();
+        Climb();
         FlipSprite();
     }
 
     private void Move()
     {
         float controlInput = Input.GetAxis("Horizontal");
-        myRigidBody.velocity = new Vector2(controlInput * moveSpeed, myRigidBody.velocity.y);
+        playerRigidBody.velocity = new Vector2(controlInput * moveSpeed, playerRigidBody.velocity.y);
 
-        bool playerHasHorizontalSpeed = Mathf.Abs(myRigidBody.velocity.x) > Mathf.Epsilon;
-        if (playerHasHorizontalSpeed)
-        {
-            playerAnimator.SetBool("IsRunning", true);
-        }
-        else
-        {
-            playerAnimator.SetBool("IsRunning", false);
-        }
+        bool playerHasHorizontalSpeed = Mathf.Abs(playerRigidBody.velocity.x) > Mathf.Epsilon;
+        playerAnimator.SetBool("IsRunning", playerHasHorizontalSpeed);
     }
 
     private void Jump()
@@ -51,19 +48,37 @@ public class Player : MonoBehaviour
         if (Input.GetButtonDown("Jump"))
         {
             Vector2 jumpVelocityToAdd = new Vector2(0f, jumpSpeed);
-            myRigidBody.velocity += jumpVelocityToAdd;
+            playerRigidBody.velocity += jumpVelocityToAdd;
         }
+    }
+
+    private void Climb()
+    {
+        if (!playerCollider2D.IsTouchingLayers(LayerMask.GetMask("Ladder")))
+        {
+            playerAnimator.SetBool("IsClimbing", false);
+            playerRigidBody.gravityScale = gravityScaleAtStart;
+            return;
+        }
+
+        float verticalInput = Input.GetAxis("Vertical");
+        Vector2 climbVelocity = new Vector2(playerRigidBody.velocity.x, verticalInput * climbSpeed);
+        playerRigidBody.velocity = climbVelocity;
+
+        bool playerHasVerticalSpeed = Mathf.Abs(playerRigidBody.velocity.y) > Mathf.Epsilon;
+        playerAnimator.SetBool("IsClimbing", playerHasVerticalSpeed);
+        playerRigidBody.gravityScale = 0;
     }
 
     private void FlipSprite()
     {
-        bool playerHasHoriztonalSpeed = Mathf.Abs(myRigidBody.velocity.x) > Mathf.Epsilon;
+        bool playerHasHoriztonalSpeed = Mathf.Abs(playerRigidBody.velocity.x) > Mathf.Epsilon;
 
         // if the player is moving horizontally
         if (playerHasHoriztonalSpeed)
         {
             // -ve makes the player sprite face left; +ve makes player sprite face right
-            transform.localScale = new Vector2(Mathf.Sign(myRigidBody.velocity.x),1f);
+            transform.localScale = new Vector2(Mathf.Sign(playerRigidBody.velocity.x),1f);
         }
     }
 }
