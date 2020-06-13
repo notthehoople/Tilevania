93525bafb74a11cf87be5797c562068184b88ebf
Mark Bradshaw <notthehoople@googlemail.com>
2020-06-13 18:28:58 +0100

    Additional files for enemy addition
    Unity failed to include these files in the previous commit.

---
+++ Assets/Scripts/Enemy.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Enemy : MonoBehaviour
+{
+    // Configuration Parameters
+    [SerializeField] float moveSpeed = 1f;
+
+    // Cached References
+    Rigidbody2D enemyRigidBody;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        enemyRigidBody = GetComponent<Rigidbody2D>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (IsFacingRight())
+        {
+            enemyRigidBody.velocity = new Vector2(moveSpeed, 0f);
+        }
+        else
+        {
+            enemyRigidBody.velocity = new Vector2(-moveSpeed, 0f);
+        }
+    }
+
+    private bool IsFacingRight()
+    {
+        // If the enemy is facing left then we've made the localScale of x negative
+        return transform.localScale.x > 0;
+    }
+
+    private void OnTriggerExit2D(Collider2D collision)
+    {
+        // Flip the enemy sprite
+        transform.localScale = new Vector2(-(Mathf.Sign(enemyRigidBody.velocity.x)), 1f);
+    }
+}
