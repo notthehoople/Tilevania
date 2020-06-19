1368107deac47bd8139522be5fc1269a505da066
Mark Bradshaw <notthehoople@googlemail.com>
2020-06-19 10:01:15 +0100

    Added sound effect to coin pickup
    - added audio clip to coin prefab
    - playclipatpoint in coinpickup.cs
    - BUGFIX: corrected sticking point for player in foreground tiles

--- Assets/Scripts/CoinPickup.cs
+++ Assets/Scripts/CoinPickup.cs
@@ -4,8 +4,14 @@ using UnityEngine;
 
 public class CoinPickup : MonoBehaviour
 {
+    // Configuration Parameters
+    [SerializeField] AudioClip coinPickupSFX;
+
     private void OnTriggerEnter2D(Collider2D collision)
     {
+        // We need to play the sound clip at the position of the main camera
+        // Otherwise it plays at the position of the coin which is far away
+        AudioSource.PlayClipAtPoint(coinPickupSFX, Camera.main.transform.position);
         Destroy(gameObject);
     }
 }
