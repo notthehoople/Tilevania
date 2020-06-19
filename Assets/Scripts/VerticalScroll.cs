a8335b50fd89b932c80094e08814cfb90a969870
Mark Bradshaw <notthehoople@googlemail.com>
2020-06-19 13:10:14 +0100

    Added rising water & tidy up
    - added water tilemap and water
    - added script to raise the water level slowly to add threat to the player
    - tidied up the hierarchy in levels 1 & 2
    - added Scene Persist prefab to level 2

---
+++ Assets/Scripts/VerticalScroll.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class VerticalScroll : MonoBehaviour
+{
+    // Configuration Parameters
+    [Tooltip ("Game units per second")]
+    [SerializeField] float scrollRate = 0.001f;
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.Translate(new Vector3(0, scrollRate * Time.deltaTime, 0));
+    }
+}
